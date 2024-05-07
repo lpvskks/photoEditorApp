1816161cd1330fafd9d19003bf5f137c2ad14df4
lpvskks <lipvskks030806@gmail.com>
2024-05-07 23:31:24 +0300

    camera

--- app/src/main/java/com/example/photoeditorapplication/MainActivity.kt
+++ app/src/main/java/com/example/photoeditorapplication/MainActivity.kt
@@ -9,15 +9,21 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val GALLERY_REQUEST_CODE = 100
     private val PERMISSION_REQUEST_CODE = 101
+    private val CAMERA_REQUEST_CODE = 102
+    private val CAMERA_PERMISSION_CODE = 103
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +33,24 @@ class MainActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
         }
 
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
+        val galleryButton = findViewById<Button>(R.id.button)
+        galleryButton.setOnClickListener {
             checkPermissionAndOpenGallery()
+        }
+
+        val cameraButton = findViewById<ImageButton>(R.id.cameraButton)
+        cameraButton.setOnClickListener {
+            if (checkCameraPermission()) {
+                openCamera()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+            }
         }
     }
 
     private fun checkPermissionAndOpenGallery() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                // Тут будет штука-объяснение, зачем нужно разрешение
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
-            }
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
         } else {
             openGallery()
         }
@@ -50,17 +61,26 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, GALLERY_REQUEST_CODE)
     }
 
+    private fun checkCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    }
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openGallery()
-            } else {
-                Toast.makeText(this, "Разрешение на доступ к галерее необходимо!:(", Toast.LENGTH_SHORT).show()
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", packageName, null))
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                startActivity(intent)
-            }
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openGallery()
+        } else if (requestCode == CAMERA_PERMISSION_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera()
+        } else {
+            Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", packageName, null))
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
         }
     }
 
@@ -68,13 +88,25 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
             val selectedImageUri = data.data
-
             val editIntent = Intent(this, ImageEditActivity::class.java).apply {
                 putExtra("imageUri", selectedImageUri)
+            }
+            startActivity(editIntent)
+        } else if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            val photo = data.extras?.get("data") as Bitmap
+            // Assume you have a method to save a bitmap and get a Uri
+            val photoUri = saveImageTemporary(photo)
+            val editIntent = Intent(this, ImageEditActivity::class.java).apply {
+                putExtra("imageUri", photoUri)
             }
             startActivity(editIntent)
         }
     }
 
+    private fun saveImageTemporary(bitmap: Bitmap): Uri? {
+        val bytes = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(contentResolver, bitmap, "New Photo", null)
+        return Uri.parse(path)
+    }
 }
-
